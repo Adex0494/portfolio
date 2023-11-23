480d38af3af7acb3bfe0dac7482cf267c4158c20
Ariangel <adex0494@gmail.com>
2023-11-23 17:19:37 -0400

    About content refactored

--- src/constants/about.tsx
+++ src/constants/about.tsx
@@ -1,239 +1,251 @@
-export const fullName = 'Ariangel Díaz Espaillat'
+export const fullName = "Ariangel Díaz Espaillat";
 
-export const birthDate = 'October 4th, 1994'
+export const birthDate = "October 4th, 1994";
 
 export const contacts = {
-    email: {
-        href:'mailto: adex0494@gmail.com',
-        text:'adex0494@gmail.com'
-    },
-    location: {
-        href:'https://www.google.com/maps/place/Bella+Vista,+Santiago+De+Los+Caballeros+51000/@19.4430748,-70.7087802,16z/data=!3m1!4b1!4m6!3m5!1s0x8eb1cf4fc363a529:0xbc6a912085e7ddaa!8m2!3d19.4453668!4d-70.7094127!16s%2Fg%2F1td2vz26?entry=ttu',
-        text:'Bella Vista, Santiago, 51000, Dominican Republic'
-    },
-    tel: {
-        href:'tel:1-809-910-4977',
-        text:'1-809-910-4977'
-    },
-    linkedIn: {
-        href:'https://www.linkedin.com/in/ariangel-d%C3%ADaz-espaillat-531171168/',
-        text: 'LinkedIn profile',
-    },
-    github: {
-        href:'https://github.com/Adex0494',
-        text: 'Github profile'
-    }
-}
+  email: {
+    href: "mailto: adex0494@gmail.com",
+    text: "adex0494@gmail.com",
+  },
+  location: {
+    href: "https://www.google.com/maps/place/Bella+Vista,+Santiago+De+Los+Caballeros+51000/@19.4430748,-70.7087802,16z/data=!3m1!4b1!4m6!3m5!1s0x8eb1cf4fc363a529:0xbc6a912085e7ddaa!8m2!3d19.4453668!4d-70.7094127!16s%2Fg%2F1td2vz26?entry=ttu",
+    text: "Bella Vista, Santiago, 51000, Dominican Republic",
+  },
+  tel: {
+    href: "tel:1-809-910-4977",
+    text: "1-809-910-4977",
+  },
+  linkedIn: {
+    href: "https://www.linkedin.com/in/ariangel-d%C3%ADaz-espaillat-531171168/",
+    text: "LinkedIn profile",
+  },
+  github: {
+    href: "https://github.com/Adex0494",
+    text: "Github profile",
+  },
+};
 
 export const links = [
-    ['JavaScript Certificate', 'https://www.udemy.com/certificate/UC-4df68261-536a-4734-9ab4-fbf5be6942b2/'],
-    ['React.js Certificate','https://www.udemy.com/certificate/UC-d39e6e98-a8a9-4505-b33e-d029ac3c7399/'],
-    [contacts.github.text, contacts.github.href],
-    [contacts.linkedIn.text, contacts.linkedIn.href]
-]
+  [
+    "JavaScript Certificate",
+    "https://www.udemy.com/certificate/UC-4df68261-536a-4734-9ab4-fbf5be6942b2/",
+  ],
+  [
+    "React.js Certificate",
+    "https://www.udemy.com/certificate/UC-d39e6e98-a8a9-4505-b33e-d029ac3c7399/",
+  ],
+  [contacts.github.text, contacts.github.href],
+  [contacts.linkedIn.text, contacts.linkedIn.href],
+];
 
 export const skills = [
-    {
-        skill: 'React.js',
-        level: 5
-    },
-    {
-        skill: 'JavaScript',
-        level: 5
-    },
-    {
-        skill: 'CSS',
-        level: 4
-    },
-    {
-        skill: 'HTML',
-        level: 4
-    },
-    {
-        skill: 'Material UI',
-        level: 4
-    },
-    {
-        skill: 'Redux',
-        level: 4
-    },
-    {
-        skill: 'Agile Methodologies',
-        level: 5
-    },
-    {
-        skill: 'Scrum',
-        level: 5
-    },
-    {
-        skill: 'Data Analysis',
-        level: 4
-    },
-    {
-        skill: 'Leadership and Teamwork',
-        level: 5
-    },
-    {
-        skill: 'Communication',
-        level: 5
-    },
-    {
-        skill: 'Organization',
-        level: 5
-    },
-]
+  {
+    skill: "React.js",
+    level: 5,
+  },
+  {
+    skill: "JavaScript",
+    level: 5,
+  },
+  {
+    skill: "Next.js",
+    level: 2,
+  },
+  {
+    skill: "CSS",
+    level: 4,
+  },
+  {
+    skill: "HTML",
+    level: 4,
+  },
+  {
+    skill: "Material UI",
+    level: 4,
+  },
+  {
+    skill: "Redux",
+    level: 4,
+  },
+  {
+    skill: "Agile Methodologies",
+    level: 5,
+  },
+  {
+    skill: "Scrum",
+    level: 5,
+  },
+  {
+    skill: "Data Analysis",
+    level: 4,
+  },
+  {
+    skill: "Leadership and Teamwork",
+    level: 5,
+  },
+  {
+    skill: "Communication",
+    level: 5,
+  },
+  {
+    skill: "Organization",
+    level: 5,
+  },
+];
 
 export const languages = [
-    {
-        language: 'English',
-        level: 4
-    },
-    {
-        language: 'Spanish',
-        level: 5
-    }
-]
+  {
+    language: "English",
+    level: 4,
+  },
+  {
+    language: "Spanish",
+    level: 5,
+  },
+];
 
-export const hobbies = 'Chess, basketball, and dancing'
+export const hobbies = "Chess, basketball, and dancing";
 
 export const description = [
-    'Experienced JavaScript developer with 3 yars in the industry. Proficient with React.js',
-    'Used problem solving aptitude to enhance application performance by 17%.',
-    'Created data visualization tools and integrated designs. Increased speed of product life cycle and 100% of the time delivered projects within deadline.',
-    'Led a development team in all phases of the production of a web application, and reduced estimated delivery time from 6 months to 4 months. The product has helped thousands of users.'
-
-]
+  "With more than 4 years of experience in the Technology Industry, I specialize in frontend development.",
+  "I enjoy developing captivating web applications that prioritize high-fidelity designs and optimal performance.",
+  "My expertise lies in using React to seamlessly translate intricate design concepts into user-friendly interfaces.",
+  "Throughout my career, I have enhanced the performance and the user experiences of applications with effective problem-solving skills. Guiding development teams in all web application phases, I have achieved substantial delivery time reductions, resulting in an impactful product for thousands of users.",
+];
 
 export const employmentHistory = [
-    {
-        company: 'Newtech Global',
-        period: 'November 2021 - Present',
-        role: 'React.js Developer',
-        description: [
-            'Increased performance of a web application that handles advertisement for Google, Facebook and Microsoft by 17%. This app is in https://app.websitebuilder.com/adspend.',
-            'Used understanding of React.js fundamentals to promote better component lifecycle practices. Increased delivery speed by 21% with 100% deadline adherence.',
-            'Communicated with other teams and senior management to adapt to changing industry standards.',
-            'I have worked for important international clients, such as Newfold, Verizon, and Fripick.'
-        ]
-    },
-    {
-        company: 'Farmahorro',
-        period: 'Mar, 2021 - Oct, 2021',
-        role: 'React.js Developer',
-        description: [
-            'Migrated a POS application developed with Java to React.js. Sales increased by 14%.'
-        ]
-    },
-    {
-        company: 'Intellisys D. Corp.',
-        period: 'Mar, 2020 - Mar, 2021',
-        role: 'React.js Developer',
-        description: [
-            'Developed a Chat Bot for a web application that served as a medical assistant for patients that had COVID-19. Thousands of people were helped.',
-            'Performed testing, installation, configuration and troubleshooting of software.'
-        ]
-    },
-    {
-        company: 'Advansys',
-        period: 'Aug, 2018 - Aug, 2019',
-        role: 'Project Manager',
-        description: [
-            'Managed the development of a Financial Software and implemented it on 5 different Banks in the Dominican Republic. Then, these institutions increased their capital flow and improved the organization and performance of their activities',
-            'Developed 100+ queries in Microsoft SQL Server.',
-            'Had effective communication with clients. Documented requirements. Estimated development time. Tested. Implemented software installation.'
-        ]
-    },
-]
+  {
+    company: "Newtech Global",
+    period: "November 2021 - Present",
+    role: "React.js Developer",
+    description: [
+      "Led a comprehensive migration from Java to React for a platform catering to over 10,000 users, resulting in improved efficiency and user experience.",
+      "Elevated the performance of a platform responsible for managing advertising campaigns for industry leaders such as Google, Facebook, and Microsoft, achieving a 17% performance enhancement.",
+      "Implemented best practices, resulting in a remarkable acceleration in delivery speed while maintaining an adherence to project deadlines.",
+      "Optimized the platform's performance, achieving a 40% improvement in response times and reducing server costs by 20% through efficient algorithm implementations and database query optimizations.",
+    ],
+  },
+  {
+    company: "Farmahorro",
+    period: "Mar, 2021 - Oct, 2021",
+    role: "React.js Developer",
+    description: [
+      "Developed frontend solutions using React, resulting in the creation of intuitive user interfaces and optimized user experiences.",
+      "Migrated a Java-based POS application to React.js, resulting in a 25% increase in sales.",
+      "Contributed to the improvement of the scalability of the product, implementing optimizations to enhance overall performance and user experience.",
+      "Collaborated with clients during the development process, ensuring high levels of satisfaction by confirming creative proposals and implementing best practices in design and functionality.",
+    ],
+  },
+  {
+    company: "Intellisys D. Corp.",
+    period: "Mar, 2020 - Mar, 2021",
+    role: "React.js Developer",
+    description: [
+      "Developed a Chat Bot within a web application, functioning as a medical assistant for COVID-19 patients and providing aid to thousands.",
+      "Conducted comprehensive software tasks including testing, installation, configuration, and adept troubleshooting.",
+      "Provided suggestions for new technologies integration, resulting in a 20% increase in system performance and alignment with business goals.",
+    ],
+  },
+  {
+    company: "Advansys",
+    period: "Aug, 2018 - Aug, 2019",
+    role: "Project Manager",
+    description: [
+      "Managed the development of a Financial Software and implemented it on 5 different Banks in the Dominican Republic. This initiative substantially amplified their capital flow and ushered in heightened operational efficiency.",
+      "Developed 100+ queries within Microsoft SQL Server, demonstrating a profound command over database management and optimization.",
+      "Worked with clients, building strong relationships and documenting their needs to subsequently create solutions aligned with those requirements.",
+    ],
+  },
+];
 
 export const educationHistory = [
-    {
-        company: 'Pontificia Universidad Católica Madre y Maestra (PUCMM)',
-        period: 'Aug, 2012 - May, 2018',
-        role: 'Telematic Engineer',
-        description: [
-            'Acquired knowledge in Software Development, Telecommunications and Networks.',
-            'Created a waiter robot, whose computer was a Raspberry pi 3 model B, programmed in Python.',
-
-        ]
-    },
-]
+  {
+    company: "Pontificia Universidad Católica Madre y Maestra (PUCMM)",
+    period: "Aug, 2012 - May, 2018",
+    role: "Telematic Engineer",
+    description: [
+      "Acquired knowledge in Software Development, Telecommunications and Networks.",
+      "Created a waiter robot, whose computer was a Raspberry pi 3 model B, programmed in Python.",
+    ],
+  },
+];
 
 export const coursesHistory = [
-    {
-        company: 'Udemy',
-        period: 'Feb, 2021 - May 2021',
-        role: 'Advanced JavaScript',
-        description: null
-    },
-    {
-        company: 'Udemy',
-        period: 'Apr, 2021 - Jun, 2021',
-        role: 'Advanced React.js',
-        description: null
-    },
-    {
-        company: 'Udemy',
-        period: 'May, 2021 - Jul, 2021',
-        role: 'Node.js',
-        description: null
-    },
-    {
-        company: 'Udemy',
-        period: 'Aug, 2020 - Feb, 2021',
-        role: 'Flutter and Dart Certification',
-        description: null
-    },
-    {
-        company: 'Udemy',
-        period: 'May, 2020 - Apr, 2020',
-        role: 'Product Managment Nanodegree Program',
-        description: null
-    },
-    {
-        company: 'Udemy',
-        period: 'Jun, 2020 - Aug, 2020',
-        role: 'Agile Meets Design Thinking',
-        description: null
-    },
-]
+  {
+    company: "Udemy",
+    period: "Feb, 2021 - May 2021",
+    role: "Advanced JavaScript",
+    description: null,
+  },
+  {
+    company: "Udemy",
+    period: "Apr, 2021 - Jun, 2021",
+    role: "Advanced React.js",
+    description: null,
+  },
+  {
+    company: "Udemy",
+    period: "May, 2021 - Jul, 2021",
+    role: "Node.js",
+    description: null,
+  },
+  {
+    company: "Udemy",
+    period: "Aug, 2020 - Feb, 2021",
+    role: "Flutter and Dart Certification",
+    description: null,
+  },
+  {
+    company: "Udemy",
+    period: "May, 2020 - Apr, 2020",
+    role: "Product Managment Nanodegree Program",
+    description: null,
+  },
+  {
+    company: "Udemy",
+    period: "Jun, 2020 - Aug, 2020",
+    role: "Agile Meets Design Thinking",
+    description: null,
+  },
+];
 
 export const extraHistory = [
-    {
-        company: 'John F. Kennedy institute',
-        period: 'Jan, 2013 - Aug, 2013',
-        role: 'English teacher',
-        description: null
-    },
-]
+  {
+    company: "John F. Kennedy institute",
+    period: "Jan, 2013 - Aug, 2013",
+    role: "English teacher",
+    description: null,
+  },
+];
 
 export const intershipHistory = [
-    {
-        company: 'Edenorte',
-        period: 'Jan, 2018 - Apr, 2018',
-        role: 'Network Support',
-        description: null
-    }
-]
+  {
+    company: "Edenorte",
+    period: "Jan, 2018 - Apr, 2018",
+    role: "Network Support",
+    description: null,
+  },
+];
 
 export const history = [
-    {
-        name: 'Employment History',
-        history: employmentHistory
-    },
-    {
-        name: 'Education',
-        history: educationHistory
-    },
-    {
-        name: 'Courses',
-        history: coursesHistory
-    },
-    {
-        name: 'Extra-curricular Activities',
-        history: extraHistory
-    },
-    {
-        name: 'Interships',
-        history: intershipHistory
-    }
-]
+  {
+    name: "Employment History",
+    history: employmentHistory,
+  },
+  {
+    name: "Education",
+    history: educationHistory,
+  },
+  {
+    name: "Courses",
+    history: coursesHistory,
+  },
+  {
+    name: "Extra-curricular Activities",
+    history: extraHistory,
+  },
+  {
+    name: "Interships",
+    history: intershipHistory,
+  },
+];
